c1c0fe96d6617de2a4d915f9d627bc277f607b61
pjione <a41924250@gmail.com>
2023-04-29 02:31:18 +0900

    1. 멤버 mvc 메모리에 저장하는 방식으로 구현 2. 디비 세팅

--- src/main/java/com/example/spring3/controller/MemberController.java
+++ src/main/java/com/example/spring3/controller/MemberController.java
@@ -1,7 +1,13 @@
 package com.example.spring3.controller;
 
+import com.example.spring3.domain.Member;
 import com.example.spring3.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -9,5 +15,24 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(Member member){
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
