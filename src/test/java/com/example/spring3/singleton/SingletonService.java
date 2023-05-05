3072fbdbc09f834b73c639196daa6e002f37c542
pjione <a41924250@gmail.com>
2023-05-05 14:47:17 +0900

    싱글톤 패턴 설계해보기

---
+++ src/test/java/com/example/spring3/singleton/SingletonService.java
@@ -0,0 +1,12 @@
+package com.example.spring3.singleton;
+
+public class SingletonService {
+    private static final SingletonService instance = new SingletonService();
+    private SingletonService(){
+
+    }
+    public static SingletonService getInstance(){
+        return instance;
+    }
+
+}
