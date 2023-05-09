5b8f6164613f369eee33389168c2f6c7e430dbaf
pjione <a41924250@gmail.com>
2023-05-10 02:21:28 +0900

    리퀘스트 스코프와 provider

---
+++ src/main/java/com/example/spring3/controller/LogDemoController.java
@@ -0,0 +1,31 @@
+package com.example.spring3.controller;
+
+import com.example.spring3.common.MyLogger;
+import com.example.spring3.service.LogDemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURI().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "ok";
+    }
+}
